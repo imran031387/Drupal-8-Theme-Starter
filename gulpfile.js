8fd25f55a4969c40af816c0e2d5134ef0f1e3aab
Mohammed Imran <mimran@mimran.local>
2017-06-19 12:54:43 +0530

    Updated the live reload and minify features

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,52 @@
 var gulp = require('gulp');
-var livereload = require('gulp-livereload')
-var uglify = require('gulp-uglifyjs');
+var livereload = require('gulp-livereload');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 
 
 
 gulp.task('imagemin', function () {
-    return gulp.src('./themes/custom/d8theme_starter/images/*')
+    return gulp.src('./images/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         }))
-        .pipe(gulp.dest('./themes/custom/d8theme_starter/images'));
+        .pipe(gulp.dest('./images'));
 });
 
 
 gulp.task('sass', function () {
-  gulp.src('./themes/custom/d8theme_starter/sass/**/*.scss')
+  gulp.src('./sass/**/*.scss')
     .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./themes/custom/d8theme_starter/css'));
+    .pipe(gulp.dest('./css'));
 });
 
 
 gulp.task('uglify', function() {
-  gulp.src('./themes/custom/d8theme_starter/lib/*.js')
-    .pipe(uglify('main.js'))
-    .pipe(gulp.dest('./themes/custom/d8theme_starter/js'))
+    return gulp.src('./lib/*.js')
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('./js'))
+        .pipe(rename('main.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./js'));
 });
 
 gulp.task('watch', function(){
     livereload.listen();
-
-    gulp.watch('./themes/custom/d8theme_starter/sass/**/*.scss', ['sass']);
-    gulp.watch('./themes/custom/d8theme_starter/lib/*.js', ['uglify']);
-    gulp.watch(['./themes/custom/d8theme_starter/css/style.css', './themes/custom/d8theme_starter/**/*.twig', './themes/custom/d8theme_starter/js/*.js'], function (files){
+    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./lib/*.js', ['uglify']);
+    gulp.watch(['./css/style.css', './**/*.twig', './js/*.js'], function (files){
         livereload.changed(files)
     });
 });
